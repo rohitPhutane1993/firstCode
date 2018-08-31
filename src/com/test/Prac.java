c54643cad012d50759e87670632696bf1d8aefa7
Rohit Phutane <rohit@icreon.com>
2018-08-31 16:31:48 +0530

    Add

--- src/com/test/Prac.java
+++ src/com/test/Prac.java
@@ -4,6 +4,8 @@ public class Prac {
 	
  public static void main(String[] args) {
 	System.out.println("Hello World");
+        System.out.println("Hello World");
+System.out.println("Hello World");
 	 
 }
 
